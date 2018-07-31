9c0b74d67d1dc5c54a5e511ec61c952a3a91144b
Henry Vo <henry.q.vo@pwc.com>
2018-07-31 22:33:36 +1000

    Adds vocab to file and randomises 10 words

--- translate.js
+++ translate.js
@@ -1,12 +1,27 @@
-const words = require('./vocabulary');
+const wordData = require('./vocabulary');
 
 const translate = require('google-translate-api');
+
+const words = [];
+const wordDataCopy = wordData;
+
+for (let i=0; i<10; i++) {
+    const random = Math.floor(Math.random() * wordDataCopy.length);
+    const randomWord = wordDataCopy[random];
+
+    const index = wordDataCopy.indexOf(randomWord);
+    wordDataCopy.splice(index, 1);
+
+    words.push(randomWord);
+}
+
+console.log(words);
 
 words.forEach((word) => {
     translate(word, {to: 'fr'}).then(res => {
         console.log(res.text);
         // console.log(res.from.language.iso);
-    }).catch(err => {
+    }).catch(err    => {
         console.error(err);
     });
 })
